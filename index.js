48e27065694615e659eebe3006ec699bad777b56
gabidavila <gabidavila@gmail.com>
2017-08-29 16:55:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter += 1;
+    return counter;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType) {
+  return function(deadlyDevice) {
+    return {animalType: animalType, deadlyDevice: deadlyDevice}
+  }
+}
+
+let sharkCreator = createAnimal("Shark")
+let sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+let sharkWithFrickinCannon = sharkCreator("Cannon")
